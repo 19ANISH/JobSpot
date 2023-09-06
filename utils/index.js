cb5a72c6bf8abf84cc76d14e579d0ed495e705a6
Anish Rajwani <75964487+19ANISH@users.noreply.github.com>
2023-09-06 15:42:31 +0530

    Updated file /utils/index.js

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,7 @@
-new
+export const checkImageURL = (url) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        return pattern.test(url);
+    }
+};
